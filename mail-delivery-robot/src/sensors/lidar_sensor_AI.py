156c93f818f5fe65fe07cca66b60d99c3f4d4c92
Connor Yelle <yelleconnor@gmail.com>
2026-01-22 14:28:22 -0500

    added metric for amount of AI fallbacks, This is modular because any AI node can write to the logging file that tracks AI fallbacks

--- mail-delivery-robot/src/sensors/lidar_sensor_AI.py
+++ mail-delivery-robot/src/sensors/lidar_sensor_AI.py
@@ -41,6 +41,14 @@ class LidarSensor(Node):
         self.right_distances = []
         self.left_distances = []
         self.front_distances = []
+        
+        self.fallback_log_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        '../../tools/logs/ai_fallback_log.txt'
+        )
+
+        # Ensure directory exists
+        os.makedirs(os.path.dirname(self.fallback_log_path), exist_ok=True)
 
         # Fallback logging
         self.fallback_log_path = "/home/hari-admin/testing_ws/Carleton-Mail-Delivery-Robot/mail-delivery-robot/src/tools/logs/ai_fallback_log.txt"
@@ -182,7 +190,7 @@ class LidarSensor(Node):
         )
 
         thread.start()
-        thread.join(timeout=10.0)  # ⏱️ 10s timeout
+        thread.join(timeout=10.0)
 
         if thread.is_alive():
             self._log_fallback("TIMEOUT")
