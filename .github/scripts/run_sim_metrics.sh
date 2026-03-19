0c06871f3c3c0ff972693b7d1d67e92c67202531
tommy <tommyphang@cmail.carleton.ca>
2026-03-18 20:05:14 -0400

    fix BT

--- .github/scripts/run_sim_metrics.sh
+++ .github/scripts/run_sim_metrics.sh
@@ -188,7 +188,7 @@ timeout "${RUN_TIMEOUT_SECONDS}" \
   use_ai_lidar:="${USE_AI_LIDAR}" \
   use_ai_navigation:="${USE_AI_NAVIGATION}" \
   use_ai_beacon:="${USE_AI_BEACON}" \
-  use_fake_beacons:="false" \
+  use_fake_beacons:="true" \
   use_ai_avoidance:="${USE_AI_AVOIDANCE}" \
   use_ai_travel_layer:="${USE_AI_TRAVEL_LAYER}" \
   2>&1 | tee /tmp/robot.log
